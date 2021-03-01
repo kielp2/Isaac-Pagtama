43fe581977bca28a30d12346a7e5cf91cc58cdb7
IsaacP <44852489+kielp2@users.noreply.github.com>
2021-03-01 17:04:41 -0600

    created a function that connects to my database

--- hw1_main.py
+++ hw1_main.py
@@ -1,1 +1,25 @@
-print ("hello")
+import mysql.connector
+from mysql.connector import Error
+import datetime
+from datetime import date
+
+#we want to write a funtion that handles creation of connection
+def create_connection(host_name,user_name, user_password, db_name):
+    connector = None
+    #wrap the following into a try except statement
+    connection = mysql.connector.connect(
+        host = host_name,
+        user = user_name,
+        passwd = user_password,
+        database = db_name
+    )
+
+    print("Connection to MySQL DB successful")
+
+    return connection
+
+    #open up connection
+#call funtion to open connection
+connection = create_connection("ip-cis3368-db.cn1eadcsnzzp.us-east-1.rds.amazonaws.com","kielp2"
+, "easyas123", "IsaacDB")
+
